eca5829062ee7aae356b0e8313228ccbf95b2aac
hschne <hans.schnedlitz@gmail.com>
2019-04-26 00:10:11 +0200

    Implement getting relative file path

--- gatsh.sh
+++ gatsh.sh
@@ -6,31 +6,45 @@ main() {
 }
 
 load_file() {
-  local path="$1"
+  local root_file
+  root_file=$(realpath "$1")
   local contents
-  contents=$(cat "$path") 
-  get_source_lines "$path" "$contents"
+  contents=$(cat "$root_file") 
+  get_source_lines "$root_file" "$contents"
 }
 
 get_source_lines() {
-  local path="$1"
+  local root_file="$1"
   local contents="$2"
   local sources
   sources=$(echo "$contents" | sed '/^#!/d' \
-    | grep -e 'source\s' \
-    | cut -d' ' -f2- \
-    | tr ' ' '\n' )
+    | grep -e 'source\s')
   [ -z "$sources" ] && return
   while read -r line; do
       echo "... $line ..."
   done <<< "$sources"
 }
 
-get_absolute_path() {
-  local path="$1"
+parse_source() {
+  local root_file="$1"
   local source="$2"
+  local source_files
+  source_files=$(echo "$source" | cut -d' ' -f2- \
+    | tr ' ' '\n')
+  while read -r line; do
+      echo "... $line ..."
+  done <<< "$source_files"
 }
 
-[[ ${BASH_SOURCE[0]} == $0 ]] && main "$@"
+parse_source_file() {
+  local root_file="$1"
+  local source="$2"
+  local source_path
+  # Get path relative to file which sources source
+  source_path=$(cd "${root_file%/*}" && realpath "$source")
+  echo "$source_path"
+}
+
+[[ ${BASH_SOURCE[0]} == "$0" ]] && main "$@"
 
 
